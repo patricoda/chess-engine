9707a77d1b185008f9331c78278b05e08764d4eb
Patrick Craig <patrickb@olmgroup.com>
2024-03-31 18:13:33 +0100

    add sourcemap support, minify cjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,13 +4,15 @@ export default {
   input: "src/index.js",
   output: [
     {
+      sourcemap: true,
       file: "dist/bundle.js",
       format: "cjs",
+      plugins: [terser()],
     },
     {
+      sourcemap: true,
       file: "dist/bundle.min.js",
       format: "es",
-      name: "version",
       plugins: [terser()],
     },
   ],
