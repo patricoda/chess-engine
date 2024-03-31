0f8533dc70a148efb6c11a5c2e6ddfc7b380b9c0
Patrick Craig <patrickb@olmgroup.com>
2024-03-31 18:02:10 +0100

    added rollup support

---
+++ rollup.config.mjs
@@ -0,0 +1,17 @@
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/bundle.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/bundle.min.js",
+      format: "es",
+      name: "version",
+      plugins: [terser()],
+    },
+  ],
+};
